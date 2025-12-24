b8ea712469b26c36c46f5e4123be6068da83b8a4
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2025-12-24 14:20:28 +0900

    feat: build express web server

---
+++ server.js
@@ -0,0 +1,42 @@
+console.log(`Running Web Server`);
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Intro codes
+app.use(express.static("public"));  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));    
+
+// 2: Session
+
+// 3:VIews codes
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing codes
+// app.get("/hello", (req, res) =>{
+//     res.end("<h1>Hello World:</h1>");
+// });
+
+// app.get("/gifts", (req, res) =>{
+//     res.end("<h1>This page is for gifts:</h1>");
+// });
+app.post("/create-item", function (req,res)  {
+    console.log(req.body);
+    res.json({test:"success"});
+});
+
+app.get("/", function (req,res){
+    res.render("purchase");
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on PORT: ${PORT}`);  
+});
+
+
+
