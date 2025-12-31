c1a9d1af85a350afce3512f0beb3532a4f98a269
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2025-12-31 15:50:05 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -43,14 +43,14 @@ app.get(`/author`, (req, res) =>{
 })
 
 app.get("/", function (req,res){
-    res.render("purchase");
+    res.render("reja");
 });
 
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function(){
-    console.log(`The server is running successfully on PORT: ${PORT}`);  
+    console.log(`The server is running successfully on PORT: ${PORT}, http://localhost:${PORT} `);  
 });
 
 
